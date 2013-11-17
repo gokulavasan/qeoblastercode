ffe1604fee4de868f253dd75d6aa96f6308b0466
gokulavasan <gokulavasan@gmail.com>
2013-11-17 06:21:58 -0800

    camInfoApp debug msgs

--- caminfopi/camInfoApp.c
+++ caminfopi/camInfoApp.c
@@ -26,14 +26,32 @@
 #include "QCamData.h"
 
 static int kinectId = 0;
-static int numPplInRoom = 4;
+static int numPplInRoom = 0;
+static int maxNumPpl = 0;
 static char * nameUser  = "KinectPI";
 static char * roomName  = "LivingRoom";
-static char * irCommand = "PAUSE";
+static char * irCommand = "pause_play";
 
 static qeo_event_writer_t *ircmd_writer; //Publish cmds to IR
 static qeo_state_writer_t *pplstate_writer; //Publish the state to the world
 static qeo_event_reader_t *mastercmd_reader; //Read cmds from MasterApp
+static qeo_event_writer_t *mastercmd_writer; //Write Cmds to test the reader
+
+static void sendCmd()
+{
+	org_qeo_qeoblaster_qeokinect_PeoplePresenceState_t changeState; //from, room - strings, num_people - int
+	changeState.id = kinectId;
+	changeState.from = nameUser;
+	changeState.room = roomName;
+	changeState.num_people = numPplInRoom;
+	qeo_state_writer_write(pplstate_writer, &changeState); 
+	//Do some intelligent decision here about max people seen so far and depending upon the current state and the max state seen
+	//decide to send the command to irBlaster 
+	org_qeo_qeoblaster_qeoir_IRCommand_t irCmd; //from, cmd - strings
+	irCmd.from = nameUser;
+	irCmd.cmd  = irCommand;
+	qeo_event_writer_write(ircmd_writer, &irCmd);
+}
 
 /* ===[ Master App message listeners ]============================================= */
 //For Demo/Testing Purposes only :: camInfoApp subscribes to this topic only for testing/demo purposes
@@ -41,23 +59,31 @@ static void on_receive_cmd (const qeo_event_reader_t *reader,
                             const void *data,
                             uintptr_t userdata)
 {
-    org_qeo_qeoblaster_qeomastercam_MockCommand_t *msg = (org_qeo_qeoblaster_qeomastercam_MockCommand_t *)data;
-    /* Whenever a new cmd arrives, print it to stdout and issue the comand to Mock */
-    printf("Received from %s : Change people state to %d\n", msg->from, msg->number);
-    numPplInRoom = msg->number;
-    org_qeo_qeoblaster_qeokinect_PeoplePresenceState_t changeState; //from, room - strings, num_people - int
-    changeState.id = kinectId;
-    changeState.from = nameUser;
-    changeState.room = roomName;
-    changeState.num_people = numPplInRoom;
-    qeo_state_writer_write(pplstate_writer, &changeState); 
-    //Do some intelligent decision here about max people seen so far and depending upon the current state and the max state seen
-    //decide to send the command to irBlaster 
-    org_qeo_qeoblaster_qeoir_IRCommand_t irCmd; //from, cmd - strings
-    irCmd.from = nameUser;
-    irCmd.cmd  = irCommand;
-    qeo_event_writer_write(ircmd_writer, &irCmd);
+	org_qeo_qeoblaster_qeomastercam_MockCommand_t *msg = (org_qeo_qeoblaster_qeomastercam_MockCommand_t *)data;
+	/* Whenever a new cmd arrives, print it to stdout and issue the comand to Mock */
+	printf("Received from : Change people state to %d\n", msg->number);
+	if (msg->number < numPplInRoom)
+	{
+                printf("Decrease in Pop :: Sending Cmd : Before %d Now %d Max %d\n", numPplInRoom, msg->number, maxNumPpl);
+		numPplInRoom = msg->number; //Update number of ppl in room
+                sendCmd();
+	}
+        else if (msg->number > numPplInRoom)
+        {
+		if (msg->number > maxNumPpl)
+		{
+                        printf("Increase in maxPpl : Before %d Now %d\n", maxNumPpl, msg->number);
+			numPplInRoom = maxNumPpl = msg->number;
+		}
+		else
+		{
+                        printf("Increase in Pop:: Sending Cmd : Before %d Now %d Max %d\n", numPplInRoom, msg->number, maxNumPpl);
+			numPplInRoom = msg->number;
+			sendCmd();
+		}
+	}
 }
+
 
 static qeo_event_reader_listener_t _listener = { .on_data = on_receive_cmd };
 
@@ -80,6 +106,7 @@ static char *chomp(char *s)
 static void help(void)
 {
     printf("Available commands:\n");
+    printf("  /sendEvent     test your event receiver\n");
     printf("  /bye           quit irBlaster application\n");
     printf("  /help          display this help\n");
     printf("  /name <name>   change user name\n");
@@ -91,6 +118,12 @@ static void handle_command(const char *cmd,
 {
     if (0 == strcmp("bye", cmd)) {
         *done = 1;
+    }
+    if (0 == strcmp("se", cmd)) {
+        org_qeo_qeoblaster_qeomastercam_MockCommand_t msg;
+        printf("Sending se\n");
+        msg.number = 13;
+        qeo_event_writer_write(mastercmd_writer, &msg);
     }
     if (0 == strcmp("help", cmd)) {
         help();
@@ -131,6 +164,7 @@ int main(int argc, const char **argv)
 	    ircmd_writer     = qeo_factory_create_event_writer(qeo, org_qeo_qeoblaster_qeoir_IRCommand_type, NULL, 0);
 	    pplstate_writer  = qeo_factory_create_state_writer(qeo, org_qeo_qeoblaster_qeokinect_PeoplePresenceState_type, NULL, 0);
 	    mastercmd_reader = qeo_factory_create_event_reader(qeo, org_qeo_qeoblaster_qeomastercam_MockCommand_type, &_listener, 0); 
+	    mastercmd_writer = qeo_factory_create_event_reader(qeo, org_qeo_qeoblaster_qeomastercam_MockCommand_type, NULL, 0); 
 
 	    //Initialize the system with some arbit value
 	    org_qeo_qeoblaster_qeokinect_PeoplePresenceState_t initState; //from, room - strings, num_people - int
