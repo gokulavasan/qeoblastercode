f00109a3dd3ab27b447b5fd7972dc67970dc190f
chris <gammagec@gmail.com>
2013-11-17 09:42:37 +0000

    Added build script, added ignore file

---
+++ qdm/compile_qdms.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+echo "Compiling QDM Files"
+echo "QEO_HOME is $QEO_HOME"
+
+$QEO_HOME/tools/qeo-codegen/bin/qeo-codegen -l c -o ../caminfopi QCamData.xml
+$QEO_HOME/tools/qeo-codegen/bin/qeo-codegen -l c -o ../irblasterpi QIRBlaster.xml
