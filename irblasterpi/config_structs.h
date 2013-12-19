15ecdb99458dc4bd9dd7baf874d8ae6dd9668b93
Christopher Gammage <gammagec@gmail.com>
2013-12-18 23:39:17 -0800

    Added data structure for storing configuration

---
+++ irblasterpi/config_structs.h
@@ -0,0 +1,49 @@
+#ifndef CONFIG_STRUCTS_H
+#define CONFIG_STRUCTS_H
+
+#include <string>
+
+enum value_type_t
+{
+	STRING,
+	INTEGER,
+	DOUBLE
+};
+
+union value_t
+{
+	std::string str_value;
+	int int_value;
+	double double_value;
+};
+
+enum compare_type_t
+{
+	EQUAL,
+	LESS,
+	GREATER
+};
+
+struct config_struct_t
+{
+	int cause_device_id; // id of device that triggers the event
+	bool cause_state_event; // true if we are looking for a state event
+	std::string cause_name; // if a state event, then it's the state name, otherwise it's the signal name
+	
+	//**** following is used for state changes only *****************************
+	compare_type_t compare_type; // what kind of comparison are we comparing the state value to?
+	value_type_t cause_value_type; // what data type is the value we are comparing?
+	value_t cause_value; // what value are we looking to compare with?
+	//***************************************************************************
+
+	int effect_device_id;
+	bool effect_state_change; // true if the event causes a state change, otherwise it causes a signal
+	std::string effect_name; // if a state change effect, name of the state, otherwise name of the signal
+
+	//****** following is for effects that change state only ******************
+	value_type_t effect_value_type; // what data type is the effect state change?
+	value_t effect_value; // what value are setting the effect state to?
+	//*************************************************************************
+};
+
+#endif CONFIG_STRUCTS_H
